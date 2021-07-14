11464e10e69fb55141ff3767fb1b5e6ee6347ed1
Solon P Pissis <solonas13@gmail.com>
2021-07-14 14:13:09 +0200

    clean up

--- search/utils.h
+++ search/utils.h
@@ -1,3 +1,21 @@
+/**
+    bd-anchors: Bidirectional String Anchors
+    Copyright (C) 2021 Grigorios Loukides and Solon P. Pissis. 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #ifdef _USE_64
 typedef int64_t INT;
 #endif
@@ -5,5 +23,4 @@ typedef int64_t INT;
 #ifdef _USE_32
 typedef int32_t INT;
 #endif
-
 
