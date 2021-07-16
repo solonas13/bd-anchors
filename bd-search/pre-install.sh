78c19f18d5174c75eea78bebb514085274643745
Solon P Pissis <solonas13@gmail.com>
2021-07-16 09:38:24 +0200

    install

--- bd-search/pre-install.sh
+++ bd-search/pre-install.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
+wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz -P "$(pwd)"
+tar -xvf "$(pwd)"/boost_1_75_0.tar.gz
+
 tar -xvf sdsl-lite.tar.gz
 cd sdsl-lite
 ./install.sh "$(pwd)"/libsdsl
 mv libsdsl/ ..
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz
-tar -xvf boost_1_75_0.tar.gz
